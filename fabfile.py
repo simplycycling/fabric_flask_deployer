cdda7e06ec76806adc254e8862a54ad97a0bc123
Roger Sherman <rsherman@viddler.com>
2015-01-01 12:19:46 -0500

    I've made this as generic as possible. It shouldn't be difficult to modify for other environments

--- fabfile.py
+++ fabfile.py
@@ -1,44 +1,40 @@
 from fabric.api import *
+from fabric.contrib.console import confirm
+import os
 import urllib
 
 # List of hosts to push to
 # Run with env.hosts unchecked will do all four nodes sequentially. For now, we'll do them 
 # individually.
-# env.hosts = ['10.70.0.40', '10.70.0.43', '10.70.0.96', '10.70.0.95']
+# env.hosts = ['root@10.70.0.40', 'root@10.70.0.43', 'root@10.70.0.96', 'root@10.70.0.95']
 
-# The command to stop mod_jk doesn't currently work, so please go to 
-# http://10.70.0.94/modjk.php?r=10
-# and control it manually (solution is in progress)
-
-
-def deploy_ROOT():
+@hosts('10.70.x.x')
+def deploy_api0():
 
     # create target and temp dir 
-
-    sudo("mkdir -p /var/opt/apps/<current_version>/ROOT")
+    sudo("mkdir -p /path/to/apps/<current_version>/MAIN_APP")
     sudo("mkdir -p /home/rsherman/tmp/<current_version>")
 
-    # push app to host - remember to update target dir version number
-    
+    # push app to host - remember to update target dir version number    
     sudo("chown -R rsherman:rsherman /home/rsherman")
-    put("/Users/rsherman/<sanitized>/fabric/ROOT.war", "/home/rsherman/tmp/<current_version>/")
-    sudo("mv /home/rsherman/tmp/<current_version>/* /var/opt/apps/<current_version>/ROOT/")
+    # TODO:  Replace the next 'put()' with a system `scp` command instead for upload progress.
+    #os.system("scp " + srcfile + " " + destination)
+    put("/Users/rsherman/viddler/fabric/MAIN_APP.war", "/home/rsherman/tmp/<current_version>/")
+    sudo("mv /home/rsherman/tmp/<current_version>/* /path/to/apps/<current_version>/MAIN_APP/")
     sudo("rm -Rf /home/rsherman/tmp")
     
-    # Expand war file (is this necessary? Yes it is!)
+    # Expand war file (is this necessary? Not in all environments) set permissions, and clean up user home dir
+    with cd("/path/to/apps/<current_version>/MAIN_APP"):
+        sudo("unzip /path/to/apps/<current_version>/MAIN_APP/MAIN_APP.war")
+    sudo("chown -R deploy:deploy /path/to/apps/<current_version>")
+    sudo("rm -f /path/to/apps/<current_version>/MAIN_APP/MAIN_APP.war")
     
-    with cd("/var/opt/apps/<current_version>/ROOT"):
-        sudo("unzip /var/opt/apps/<current_version>/ROOT/ROOT.war")
-
-    sudo("chown -R deploy:deploy /var/opt/apps/<current_version>")
-    sudo("rm -f /var/opt/apps/<current_version>/ROOT/ROOT.war")
-    
-    # confirm/execute stopping mod_jk
-    
+    # confirm/execute stopping mod_jk    
     if confirm("Stop mod_jk now?", default=False):
-    	# stop mod_jk (e.g., "web0 - <sanitized>_a")
+    	# stop mod_jk (e.g., "web0 - viddler_a")
     	# STP, "&wa=2&vwa=2"
-    	urllib.urlopen("http://10.70.0.94/modjk.php?cmd=update&w=<sanitized>&sw=viddler_a&wf=300&wn=viddler_a&wr=&wc=&wd=0&vwn=viddler_a&vwr=&vwc=&vwd=0&wa=2&vwa=2")
+        urllib.urlopen("http://_generic_REST_command_")
+        urllib.urlopen("http://_generic_REST_command_")
     
     # confirm/execute stopping tomcats
     if confirm("Stop tomcat instance(s) now?", default=False):
@@ -47,108 +43,210 @@ def deploy_ROOT():
     	sudo("service tomcat_2 stop")
 
     # clear catalina logs
-
-    sudo("> /var/opt/tomcat_1/logs/catalina.out")
-    sudo("> /var/opt/tomcat_2/logs/catalina.out")
+    sudo("> /path/to/tomcat_1/logs/catalina.out")
+    sudo("> /path/to/tomcat_2/logs/catalina.out")
 
     # switch link to new app
-
-    sudo("rm -f /var/opt/tomcat_1/webapps/ROOT")
-    sudo("ln -s /var/opt/apps/<current_version>ROOT /var/opt/tomcat_1/webapps/ROOT")
-
-    sudo("rm -f /var/opt/tomcat_2/webapps/ROOT")
-    sudo("ln -s /var/opt/apps/<current_version>/ROOT /var/opt/tomcat_2/webapps/ROOT")
+    sudo("rm -f /path/to/tomcat_1/webapps/MAIN_APP")
+    sudo("ln -s /path/to/apps/<current_version>/MAIN_APP /path/to/tomcat_1/webapps/MAIN_APP")
+    sudo("rm -f /path/to/tomcat_2/webapps/MAIN_APP")
+    sudo("ln -s /path/to/apps/<current_version>/MAIN_APP /path/to/tomcat_2/webapps/MAIN_APP")
 
 
     # confirm switch
+    sudo("ls -l /path/to/tomcat_1/webapps")
+    sudo("ls -l /path/to/tomcat_2/webapps")
+    if confirm("Has the app been switched to the proper version? If so, start tomcat.", default=False):
+        # start tomcat
+        sudo("set -m; /etc/init.d/tomcat_1 start")
+        sudo("set -m; /etc/init.d/tomcat_2 start")
 
-    sudo("ls -l /var/opt/tomcat_1/webapps")
-    sudo("ls -l /var/opt/tomcat_2/webapps")
+    # start mod_jk (e.g., "web0 - viddler_a")
+    # ACT, "&wa=0&vwa=0"    
+    urllib.urlopen("http://_generic_REST_command_")
+    urllib.urlopen("http://_generic_REST_command_")
 
-    # start tomcat
+@hosts('10.70.x.x')
+def deploy_api1():
 
-    sudo("set -m; /etc/init.d/tomcat_1 start")
-    sudo("set -m; /etc/init.d/tomcat_2 start")
-
-    # start mod_jk (e.g., "web0 - <sanitized>_a")
-    # ACT, "&wa=0&vwa=0"
-    
-    urllib.urlopen("http://10.70.0.94/modjk.php?cmd=update&w=<sanitized>&sw=viddler_a&wf=300&wn=viddler_a&wr=&wc=&wd=0&vwn=viddler_a&vwr=&vwc=&vwd=0&wa=0&vwa=0")
-
-
-@hosts("root@10.70.0.40")
-def deploy_admin():
-
-    # First, we update ROOT
-
-    sudo("mkdir -p /var/opt/apps/<current_version>/ROOT")
+    # create target and temp dir 
+    sudo("mkdir -p /path/to/apps/<current_version>/MAIN_APP")
     sudo("mkdir -p /home/rsherman/tmp/<current_version>")
 
     # push app to host - remember to update target dir version number
-    
     sudo("chown -R rsherman:rsherman /home/rsherman")
-    put("/Users/rsherman/<sanitized>/fabric/ROOT.war", "/home/rsherman/tmp/<current_version>/")
-    sudo("mv /home/rsherman/tmp/<current_version>/* /var/opt/apps/<current_version>/ROOT/")
+    put("/Users/rsherman/viddler/fabric/MAIN_APP.war", "/home/rsherman/tmp/<current_version>/")
+    sudo("mv /home/rsherman/tmp/<current_version>/* /path/to/apps/<current_version>/MAIN_APP/")
     sudo("rm -Rf /home/rsherman/tmp")
     
-    # Expand war file (is this necessary? Yes it is!)
+    # Expand war file (is this necessary? Not in all environments)
+    with cd("/path/to/apps/<current_version>/MAIN_APP"):
+        sudo("unzip /path/to/apps/<current_version>/MAIN_APP/MAIN_APP.war")
+    sudo("chown -R deploy:deploy /path/to/apps/<current_version>")
+    sudo("rm -f /path/to/apps/<current_version>/MAIN_APP/MAIN_APP.war")
     
-    with cd("/var/opt/apps/<current_version>/ROOT"):
-        sudo("unzip /var/opt/apps/<current_version>/ROOT/ROOT.war")
-
-    sudo("chown -R deploy:deploy /var/opt/apps/<current_version>")
-    sudo("rm -f /var/opt/apps/<current_version>/ROOT/ROOT.war")
+    # confirm/execute stopping mod_jk
+    if confirm("Stop mod_jk now?", default=False):
+        # stop mod_jk (e.g., "web0 - viddler_a")
+        # STP, "&wa=2&vwa=2"
+        urllib.urlopen("http://_generic_REST_command_")
+        urllib.urlopen("http://_generic_REST_command_")
     
-    # stop tomcat
-
-    sudo("service tomcat_1 stop")
+    # confirm/execute stopping tomcats
+    if confirm("Stop tomcat instance(s) now?", default=False):
+        # stop tomcat
+        sudo("service tomcat_1 stop")
+        sudo("service tomcat_2 stop")
 
     # clear catalina logs
-
-    sudo("> /var/opt/tomcat_1/logs/catalina.out")
+    sudo("> /path/to/tomcat_1/logs/catalina.out")
+    sudo("> /path/to/tomcat_2/logs/catalina.out")
 
     # switch link to new app
-
-    sudo("rm -f /var/opt/tomcat_1/webapps/ROOT")
-    sudo("ln -s /var/opt/apps/<current_version>ROOT /var/opt/tomcat_1/webapps/ROOT")
+    sudo("rm -f /path/to/tomcat_1/webapps/MAIN_APP")
+    sudo("ln -s /path/to/apps/<current_version>/MAIN_APP /path/to/tomcat_1/webapps/MAIN_APP")
+    sudo("rm -f /path/to/tomcat_2/webapps/MAIN_APP")
+    sudo("ln -s /path/to/apps/<current_version>/MAIN_APP /path/to/tomcat_2/webapps/MAIN_APP")
 
 
     # confirm switch
+    sudo("ls -l /path/to/tomcat_1/webapps")
+    sudo("ls -l /path/to/tomcat_2/webapps")
+    if confirm("Has the app been switched to the proper version? If so, start tomcat.", default=False):
+        # start tomcat
+        sudo("set -m; /etc/init.d/tomcat_1 start")
+        sudo("set -m; /etc/init.d/tomcat_2 start")
 
-    sudo("ls -l /var/opt/tomcat_1/webapps")
+    # start mod_jk (e.g., "web0 - viddler_a")
+    # ACT, "&wa=0&vwa=0"
+    urllib.urlopen("http://_generic_REST_command_")
+    urllib.urlopen("http://_generic_REST_command_")
 
-    # start tomcat
+@hosts('10.70.0.43')
+def deploy_web0():
 
-    sudo("set -m; /etc/init.d/tomcat_1 start")
-    
-
-    # Now update admin
-
-    sudo("mkdir -p /var/opt/apps/<current_version>/<sanitized>_admin")
+    # create target and temp dir 
+    sudo("mkdir -p /path/to/apps/<current_version>/MAIN_APP")
     sudo("mkdir -p /home/rsherman/tmp/<current_version>")
 
     # push app to host - remember to update target dir version number
-    
     sudo("chown -R rsherman:rsherman /home/rsherman")
-    put("/Users/rsherman/<sanitized>/fabric/viddler_admin.war", "/home/rsherman/tmp/<current_version>/")
-    sudo("mv /home/rsherman/tmp/<current_version>/* /var/opt/apps/<current_version>/<sanitized>_admin/")
+    put("/Users/rsherman/viddler/fabric/MAIN_APP.war", "/home/rsherman/tmp/<current_version>/")
+    sudo("mv /home/rsherman/tmp/<current_version>/* /path/to/apps/<current_version>/MAIN_APP/")
     sudo("rm -Rf /home/rsherman/tmp")
-
-    with cd("/var/opt/apps/<current_version>/<sanitized>_admin"):
-        sudo("unzip /var/opt/apps/<current_version>/<sanitized>_admin/viddler_admin.war")
-
-    sudo("chown -R deploy:deploy /var/opt/apps/<current_version>")
-    sudo("rm -f /var/opt/apps/<current_version>/<sanitized>_admin/viddler_admin.war")
-    sudo("service tomcat_2 stop")
-    sudo("> /var/opt/tomcat_2/logs/catalina.out")
-    sudo("rm -f /var/opt/tomcat_2/webapps/<sanitized>_admin")
-    sudo("ln -s /var/opt/apps/<current_version>/<sanitized>_admin /var/opt/tomcat_2/webapps/viddler_admin")
-    sudo("ls -l /var/opt/tomcat_2/webapps")
-    sudo("set -m; /etc/init.d/tomcat_2 start")
-
-    # Remember to stop mod_jk
     
+    # Expand war file (is this necessary? Not in all environments)
+    with cd("/path/to/apps/<current_version>/MAIN_APP"):
+        sudo("unzip /path/to/apps/<current_version>/MAIN_APP/MAIN_APP.war")
+    sudo("chown -R deploy:deploy /path/to/apps/<current_version>")
+    sudo("rm -f /path/to/apps/<current_version>/MAIN_APP/MAIN_APP.war")
+    
+    # confirm/execute stopping mod_jk
+    if confirm("Stop mod_jk now?", default=False):
+        # stop mod_jk (e.g., "web0 - viddler_a")
+        # STP, "&wa=2&vwa=2"
+        urllib.urlopen("http://_generic_REST_command_")
+        urllib.urlopen("http://_generic_REST_command_")
+    
+    # confirm/execute stopping tomcats
+    if confirm("Stop tomcat instance(s) now?", default=False):
+        # stop tomcat
+        sudo("service tomcat_1 stop")
+        sudo("service tomcat_2 stop")
 
+    # clear catalina logs
+    sudo("> /path/to/tomcat_1/logs/catalina.out")
+    sudo("> /path/to/tomcat_2/logs/catalina.out")
+
+    # switch link to new app
+    sudo("rm -f /path/to/tomcat_1/webapps/MAIN_APP")
+    sudo("ln -s /path/to/apps/<current_version>/MAIN_APP /path/to/tomcat_1/webapps/MAIN_APP")
+    sudo("rm -f /path/to/tomcat_2/webapps/MAIN_APP")
+    sudo("ln -s /path/to/apps/<current_version>/MAIN_APP /path/to/tomcat_2/webapps/MAIN_APP")
+
+    # confirm switch
+    sudo("ls -l /path/to/tomcat_1/webapps")
+    sudo("ls -l /path/to/tomcat_2/webapps")
+    if confirm("Has the app been switched to the proper version? If so, start tomcat.", default=False):
+        # start tomcat
+        sudo("set -m; /etc/init.d/tomcat_1 start")
+        sudo("set -m; /etc/init.d/tomcat_2 start")
+
+    # start mod_jk (e.g., "web0 - viddler_a")
+    # ACT, "&wa=0&vwa=0"
+    urllib.urlopen("http://_generic_REST_command_")
+    urllib.urlopen("http://_generic_REST_command_")
+
+@hosts("10.70.0.40")
+def deploy_web3():
+
+    # First, we update MAIN_APP
+    sudo("mkdir -p /path/to/apps/<current_version>/MAIN_APP")
+    sudo("mkdir -p /home/rsherman/tmp/<current_version>")
+
+    # push app to host - remember to update target dir version number
+    sudo("chown -R rsherman:rsherman /home/rsherman")
+    put("/Users/rsherman/viddler/fabric/MAIN_APP.war", "/home/rsherman/tmp/<current_version>/")
+    sudo("mv /home/rsherman/tmp/<current_version>/* /path/to/apps/<current_version>/MAIN_APP/")
+    sudo("rm -Rf /home/rsherman/tmp")
+    
+    # Expand war file (is this necessary? Not in all environments)
+    with cd("/path/to/apps/<current_version>/MAIN_APP"):
+        sudo("unzip /path/to/apps/<current_version>/MAIN_APP/MAIN_APP.war")
+    sudo("chown -R deploy:deploy /path/to/apps/<current_version>")
+    sudo("rm -f /path/to/apps/<current_version>/MAIN_APP/MAIN_APP.war")
+
+    # Stop mod_jk
+    if confirm("Stop mod_jk now?", default=False):
+        urllib.urlopen("http://_generic_REST_command_")
+        urllib.urlopen("http://_generic_REST_command_")
+    
+    # stop tomcat
+    if confirm("Stop tomcat instance(s) now?", default=False):
+        sudo("service tomcat_1 stop")
+
+    # clear catalina logs
+    sudo("> /path/to/tomcat_1/logs/catalina.out")
+
+    # switch link to new app
+    sudo("rm -f /path/to/tomcat_1/webapps/MAIN_APP")
+    sudo("ln -s /path/to/apps/<current_version>/MAIN_APP /path/to/tomcat_1/webapps/MAIN_APP")
+
+
+    # confirm switch
+    sudo("ls -l /path/to/tomcat_1/webapps")
+
+    # Now update admin
+    sudo("mkdir -p /path/to/apps/<current_version>/secondary_app")
+    sudo("mkdir -p /home/rsherman/tmp/<current_version>")
+
+    # push app to host - remember to update target dir version number
+    sudo("chown -R rsherman:rsherman /home/rsherman")
+    put("/Users/rsherman/viddler/fabric/secondary_app.war", "/home/rsherman/tmp/<current_version>/")
+    sudo("mv /home/rsherman/tmp/<current_version>/* /path/to/apps/<current_version>/secondary_app/")
+    sudo("rm -Rf /home/rsherman/tmp")
+    with cd("/path/to/apps/<current_version>/secondary_app"):
+        sudo("unzip /path/to/apps/<current_version>/secondary_app/secondary_app.war")
+    sudo("chown -R deploy:deploy /path/to/apps/<current_version>")
+    sudo("rm -f /path/to/apps/<current_version>/secondary_app/secondary_app.war")
+    if confirm("Stop tomcat instance(s) now?", default=False):
+        sudo("service tomcat_2 stop")
+    sudo("> /path/to/tomcat_2/logs/catalina.out")
+    sudo("rm -f /path/to/tomcat_2/webapps/secondary_app")
+    sudo("ln -s /path/to/apps/<current_version>/secondary_app /path/to/tomcat_2/webapps/secondary_app")
+    sudo("ls -l /path/to/tomcat_2/webapps")
+    # confirm switch
+    sudo("ls -l /path/to/tomcat_1/webapps")
+    sudo("ls -l /path/to/tomcat_2/webapps")
+    if confirm("Has the app been switched to the proper version? If so, start tomcat.", default=False):
+        sudo("set -m; /etc/init.d/tomcat_1 start")
+        sudo("set -m; /etc/init.d/tomcat_2 start")
+
+    # Start mod_jk
+    urllib.urlopen("http://_generic_REST_command_")
+    urllib.urlopen("http://_generic_REST_command_")
+
+    
 def start_tomcat():
     sudo("set -m; /etc/init.d/tomcat_1 start")
     sudo("set -m; /etc/init.d/tomcat_2 start")
@@ -157,5 +255,3 @@ def stop_tomcat():
     sudo("service tomcat_1 stop")
     sudo("service tomcat_2 stop")
 
-def test_mod_jk():
-    urllib.urlopen("http://10.70.0.94/modjk.php?cmd=update&w=<sanitized>-search&sw=viddler-search_g&wf=100&wn=viddler_g&wr=&wc=&wd=0&vwn=viddler_g&vwr=&vwc=&vwd=0&wa=2&vwa=2")
